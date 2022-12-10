50e0d8c4c6db12c5bd65e8deb0079bb961edb697
jqb1 <jakub.koziol12@gmail.com>
2022-12-10 07:18:46 +0100

    day 10

--- 10.py
+++ 10.py
@@ -1,10 +1,42 @@
 def read_input():
     with open("/Users/jkoziol/Downloads/input.txt") as f:
-        moves = [line.rstrip() for line in f]
-    return moves
+        lines = [line.rstrip() for line in f]
+    return lines
 
 
-print(read_input())
-x1, x2, y1, y2 = 0, 0, 0, 0
-lines = [tuple(map(int, line.replace(" -> ", ",").split(','))) for line in read_input()]
-print(lines)
+X = 1  # During the first cycle, X is 1
+cycle_now = 0
+cycles_strngs = (20, 60, 100, 140, 180, 220)
+signal_strengths = []
+
+screen = ""
+line_len = 0
+for line in read_input():
+    line = line.split()
+    print(line)
+    cycles_td = 1 if line[0] == "noop" else 2
+    for cycle in range(cycles_td):
+        if any(pixel == cycle_now % 40 for pixel in (X-1, X, X+1,)):
+            screen += "#"
+        else:
+            screen += "."
+        line_len += 1
+        if cycle_now in cycles_strngs:
+            signal_strengths.append(cycle_now * X)
+
+        if line_len == 40:
+            screen += "\n"
+            line_len = 0
+        if line[0] == "noop":
+            cycle_now += 1
+            continue
+        elif line[0] == "addx":
+            cycle_now += 1
+            # check
+            if cycle == 1:
+                X += int(line[1])
+
+print(signal_strengths)
+print(sum(signal_strengths))
+# the sprite is 3 pixels wide, and the X register sets the horizontal position of the middle of that sprite.
+print(screen)
---
+++ 2021/5.py
@@ -0,0 +1,56 @@
+from collections import defaultdict
+
+
+def read_input():
+    with open("/Users/jkoziol/Downloads/input.txt") as f:
+        moves = [line.rstrip() for line in f]
+    return moves
+
+
+# print(read_input())
+x1, x2, y1, y2 = 0, 0, 0, 0
+lines = [tuple(map(int, line.replace(" -> ", ",").split(','))) for line in read_input()]
+
+indices = defaultdict(int)
+
+for line in lines:
+    x1, y1, x2, y2 = line
+    print(x1, y1, x2, y2)
+    if not (x1 == x2) and not (y1 == y2):
+        if not abs(x1 - x2) == abs(y1 - y2):
+            continue
+        else:
+            dist = abs(x1 - x2)
+            cx, cy = x1, y1
+            indices[(cx, cy)] += 1
+            for s in range(dist):
+                if x1 > x2 and y1 < y2:
+                    cx -= 1
+                    cy += 1
+                    indices[(cx, cy)] += 1
+                elif x1 < x2 and y1 < y2:
+                    cx += 1
+                    cy += 1
+                    indices[(cx, cy)] += 1
+                elif x1 < x2 and y1 > y2:
+                    cx += 1
+                    cy -= 1
+                    indices[(cx, cy)] += 1
+                elif x1 > x2 and y1 > y2:
+                    cx -= 1
+                    cy -= 1
+                    indices[(cx, cy)] += 1
+    elif x1 == x2:
+        yr = range(y1, y2+1) if y1 < y2 else range(y2, y1+1)
+        for y_ in yr:
+            indices[(x1, y_)] += 1
+    elif y1 == y2:
+        xr = range(x1, x2+1) if x1 < x2 else range(x2, x1+1)
+        for x_ in xr:
+            indices[(x_, y1)] += 1
+
+# print(indices)
+# print(len(indices))
+# print(len(indices) - len(set(indices)))
+print(indices.values())
+print(len([i for i in indices.values() if i > 1]))
