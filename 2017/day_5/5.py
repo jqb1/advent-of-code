2ead658335dd25150df8c0eb327ba443cebc956c
Jakub Kozioł <jakub.koziol12@gmail.com>
2023-12-09 14:44:37 +0100

    day9

--- 2017/day_5/5.py
+++ 2017/day_5/5.py
@@ -6,6 +6,20 @@ def read_input():
 
 def solve():
     input_ = read_input()
-    print(input_)
+    instructions = [int(x) for x in input_]
+    
+    max_i = len(instructions) - 1
+    cur_i = 0
+    steps = 0
+    while cur_i >= 0 and cur_i <= max_i:
+        cur = instructions[cur_i]
+        next_j = instructions[cur_i]
+        if next_j >= 3:
+            instructions[cur_i] = cur - 1
+        else:
+            instructions[cur_i] = cur + 1
+        cur_i = cur_i + next_j
+        steps += 1
+    print(steps)
 
 solve()
---
+++ 2023/day_9/9.py
@@ -0,0 +1,28 @@
+def read_input():
+    with open("./input.txt") as f:
+        lines = [line.rstrip() for line in f]
+    return lines
+
+
+def solve():
+    input_ = read_input()
+    res = 0
+    for line in input_:
+        line = [int(v) for v in line.split()[::-1]]
+        diff = calc_diffs(line)
+        res += diff
+    print(res)
+
+
+def calc_diffs(line):
+    diffs = []
+    all_zero = True
+    if not any(line):
+        return 0
+    for i in range(1, len(line)):
+        diffs.append(line[i] - line[i-1])
+        if diffs[-1] != 0 and all_zero:
+            all_zero = False
+    return line[-1] + calc_diffs(diffs)
+
+solve()
