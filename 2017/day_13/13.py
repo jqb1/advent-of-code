9164b735c9d860468a5c10ddc4d6931e0fea5abc
Jakub Kozioł <jakub.koziol12@gmail.com>
2024-11-16 17:56:00 +0100

    day13 2017

---
+++ 2017/day_13/13.py
@@ -0,0 +1,36 @@
+def read_input():
+    with open("./input.txt") as f:
+        lines = [line.rstrip().split(': ') for line in f]
+    return lines
+
+
+def solve():
+    inp = read_input()
+    scan_interval = {}
+    for line in inp:
+        pos, depth = map(int, line)
+        # interval at which scanner will be at position 1
+        scan_interval[pos] = (depth - 1) * 2
+    last_step = int(inp[-1][0])
+
+    step = 0
+    caught = True
+    delay = 0
+    while caught:  # part2
+        caught = False
+        while step <= last_step:
+            if step in scan_interval and (step + delay) % scan_interval[step] == 0:
+                caught = True
+                step = 0
+                delay += 1
+                break
+            step += 1
+    print(delay)
+    # part 1
+    # severity = 0
+    # for p in caught:
+    #     severity += p * scan_state[p][1]
+    # print(severity)
+
+
+solve()
