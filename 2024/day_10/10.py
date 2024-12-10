bd052c638149f5b8e77549bc6a1839a0e7279f5d
Jakub Kozioł <jakub.koziol12@gmail.com>
2024-12-10 06:57:15 +0100

    day 10

---
+++ 2024/day_10/10.py
@@ -0,0 +1,88 @@
+from utils import read_input, submit
+from collections import deque, defaultdict
+
+DIRECTION = {
+    (0, -1),
+    (0, 1),
+    (-1, 0),
+    (1, 0),
+}
+
+
+def solve():
+    lines = read_input()
+    ans = 0
+    grid = {}
+    trailheads = []
+    for r, line in enumerate(lines):
+        for c, num in enumerate(line):
+            if num == ".":
+                continue
+            num = int(num)
+            grid[r, c] = num
+            if num == 0:
+                cur_p = (r, c)
+                trailheads.append(cur_p)
+    for th in trailheads:
+        q = deque([th])
+        seen = set()
+        score = 0
+        while q:
+            cur_p = q.popleft()
+            height = grid[cur_p]
+            if cur_p in seen:
+                continue
+            seen.add(cur_p)
+            if height == 9:
+                score += 1
+                continue
+            for dr, dc in DIRECTION:
+                r, c = cur_p
+                if (r + dr, c + dc) in grid and grid[r + dr, c + dc] == height + 1:
+                    q.append((r + dr, c + dc))
+
+        print(th, score)
+        ans += score
+    submit(ans, 1)
+
+
+def solve_p2():
+    lines = read_input()
+    ans = 0
+    grid = {}
+    trailheads = []
+    for r, line in enumerate(lines):
+        for c, num in enumerate(line):
+            if num == ".":
+                continue
+            num = int(num)
+            grid[r, c] = num
+            if num == 0:
+                cur_p = (r, c)
+                trailheads.append(cur_p)
+    for th in trailheads:
+        q = deque([(th, str(th))])
+        paths = set()
+        seen = set()
+        while q:
+            cur_p, path = q.popleft()
+            height = grid[cur_p]
+            if path in seen:
+                continue
+            seen.add(path)
+            if height == 9:
+                paths.add(path)
+                continue
+            for dr, dc in DIRECTION:
+                r, c = cur_p
+                if (r + dr, c + dc) in grid and grid[r + dr, c + dc] == height + 1:
+                    q.append(((r + dr, c + dc), path + str((r + dr, c + dc))))
+
+        print(th, paths)
+        ans += len(paths)
+    submit(ans, 2)
+
+
+if __name__ == "__main__":
+    # solve()
+    solve_p2()
